f9b7cc6ee2bb437b6e5a02f8870a67a47881e54e
Ellap.delta <ellap.delta@gmail.com>
2023-11-14 11:58:09 +0200

    Documented and fixed a few issues

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import copy
 
 X = 'X'
 O = 'O'
+WIN = 1000
+TWO_ROW = 50
+SINGLE_ROW = 10
 
 
 def print_board(matrix):
+    """
+    Receives a board and prints it nicely
+    :param matrix: A list of 3 lists, each contains 3 values.
+    :return: Nothing, just prints
+    """
     hr = "----------------"
     for row in matrix:
         print(hr)
@@ -18,6 +26,10 @@ def print_board(matrix):
 
 
 def get_player_sign():
+    """
+    Here the player choose whether he wants to play as X or O
+    :return: Player's symbol and agent's symbol, in their order
+    """
     while True:
         choice = input("Pick up your sign: x/o\n")
         if choice.lower() == 'x':
@@ -29,6 +41,11 @@ def get_player_sign():
 
 
 def get_player_move(board):
+    """
+    Asks the player to provide next move, and validate the syntax and move
+    :param board: The current board
+    :return: The row and column to update in the board
+    """
     print("Your turn. What's your move?")
     print("Type x-y, x for row and y for column. From 1 to 3")
 
@@ -51,10 +68,25 @@ def get_player_move(board):
 
 
 def draw(player, board, row, column):
+    """
+    Updates the board with the player's sign in the received row and column
+    :param player: The player's sign, X or O
+    :param board: The current board
+    :param row: Row to update
+    :param column: Column to update
+    :return: Nothing, just updates the board
+    """
     board[row][column] = player
 
 
 def get_agent_move(agent, board):
+    """
+    The agent tests all possible moves, calculates their values and picks
+    The row and column that will give him the best value
+    :param agent: The agent's symbol, X or O
+    :param board: The current board
+    :return: The row and column that will work best for agent
+    """
     moves = []
 
     for row in range(3):
@@ -76,29 +108,37 @@ def get_agent_move(agent, board):
 def calculate_line_value(line):
     """
     Calculate the value of a line (row, column, or diagonal) for a player.
-    Positive values represent the player's signs, and negative values represent the agent's signs.
+    Positive values represent X signs, and negative values represent O signs.
+    :param line: A list of 3 values, can represnt row, column or diagonal
+    :return: The value of the received line
     """
     x_count = line.count(X)
     o_count = line.count(O)
     empty_count = line.count('')
 
     if x_count == 3:
-        return 1000  # X wins
+        return WIN  # X wins
     elif o_count == 3:
-        return -1000  # O wins
+        return -WIN  # O wins
     elif x_count == 2 and empty_count == 1:
-        return 50  # Two in a row for x
+        return TWO_ROW  # Two in a row for x
     elif o_count == 2 and empty_count == 1:
-        return -50  # Two in a row for o
-    elif x_count == 1 and empty_count == 2:
+        return -TWO_ROW  # Two in a row for o
+    elif x_count == SINGLE_ROW and empty_count == 2:
         return 10  # One for x
-    elif o_count == 1 and empty_count == 2:
+    elif o_count == SINGLE_ROW and empty_count == 2:
         return -10  # One for o
     else:
         return 0  # No one can win this line
 
 
 def calculate_board_value(board):
+    """
+    Returns the value that represents the current state of the board
+    Positive value is good for X. Negative value is good for O.
+    :param board: The current board
+    :return: The value of the current board
+    """
     value = 0
     for row in board:
         value += calculate_line_value(row)
@@ -121,10 +161,22 @@ def calculate_board_value(board):
 
 
 def game_won(value):
-    return value > 500 or value < -500  # Arbitrary value, if passed we can tell game's over
+    """
+    Returns whether this game is won by X or Y
+    Since 1000/-1000 represent a win, and at most the loser can gain 150 value,
+    Checking above or below win value should be sufficient
+    :param value: A value of a board
+    :return: True if the game is won by any side, false otherwise
+    """
+    return value > WIN - 3 * TWO_ROW or value < -WIN + 3 * TWO_ROW  # Arbitrary value, if passed we can tell game's over
 
 
 def game_tied(board):
+    """
+    Checks if there are no more valid moves left
+    :param board: The current board
+    :return: True if there are no more valid moves left, false otherwise
+    """
     for row in board:
         for cell in row:
             if cell == '':
@@ -133,21 +185,42 @@ def game_tied(board):
 
 
 def player_turn(board, player):
+    """
+    Represents the player's turn
+    Calls a function to get input then validate it
+    Updates the board when good input received
+    :param board: The current board
+    :param player: The player's sign, X or O
+    :return: Nothing, just updates the board
+    """
     while True:
         row, column = get_player_move(board)
         if board[row][column] == '':
-            board[row][column] = player
+            draw(player, board, row, column)
             break
         else:
             print("Cell is already taken. Try again.")
 
 
 def agent_turn(board, agent):
+    """
+    Represents agent turn, picks a move and draws it
+    :param board: The current board
+    :param agent: The agent's sign, X or O
+    :return: Nothing, just updates the board
+    """
     row, column = get_agent_move(agent, board)
-    board[row][column] = agent
+    draw(agent, board, row, column)
 
 
 def decide_start(board, agent):
+    """
+    Decides whether player starts or agent starts.
+    If agent starts, it will always pick cell [2,2] as it's the optimal choice
+    :param board: The current board
+    :param agent: The agent's sign, X or Y
+    :return: Nothing, just updates the board if needed
+    """
     choice = input("Press ! if you want the agent to play first\n")
     if choice == '!':
         draw(agent, board, 1, 1)
@@ -164,7 +237,6 @@ def main():
 
     while True:
         player_turn(board, player)
-        print_board(board)
         print_board(board)
         if game_result(board, player):
             print("Nice one!")
